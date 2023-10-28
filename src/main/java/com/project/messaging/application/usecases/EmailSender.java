5992abaeb9f25c95189be202239d04a1fe0f9ab7
luan <luanbianchine1@gmail.com>
2023-10-28 13:24:43 -0300

    feat: implements infra aws ses

--- src/main/java/com/project/messaging/application/usecases/EmailSender.java
+++ src/main/java/com/project/messaging/application/usecases/EmailSender.java
@@ -2,7 +2,9 @@ package com.project.messaging.application.usecases;
 
 import com.project.messaging.adapters.EmailSenderGateway;
 import com.project.messaging.core.usecases.EmailSenderUseCase;
+import org.springframework.stereotype.Service;
 
+@Service
 public class EmailSender implements EmailSenderUseCase {
 
     private final EmailSenderGateway emailSenderGateway;
@@ -10,8 +12,9 @@ public class EmailSender implements EmailSenderUseCase {
     public EmailSender (EmailSenderGateway emailGateway) {
         this.emailSenderGateway = emailGateway;
     }
+
     @Override
     public void send(String to, String subject, String body) {
-
+        this.emailSenderGateway.sendMail(to, subject, body);
     }
 }
---
+++ src/main/java/com/project/messaging/infra/ses/SesEmailSender.java
@@ -0,0 +1,32 @@
+package com.project.messaging.infra.ses;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.model.*;
+import com.project.messaging.adapters.EmailSenderGateway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SesEmailSender implements EmailSenderGateway {
+    private final AmazonSimpleEmailService amazonSimpleEmailService;
+    @Autowired
+    public SesEmailSender (AmazonSimpleEmailService amazonSimpleEmailService) {
+        this.amazonSimpleEmailService = amazonSimpleEmailService;
+    }
+    @Override
+    public void sendMail(String to, String subject, String body) {
+        SendEmailRequest request = new SendEmailRequest()
+                .withSource("luanalmeidadev@gmail.com")
+                .withDestination(new Destination().withToAddresses(to))
+                .withMessage(new Message()
+                        .withSubject(new Content(subject))
+                        .withBody(new Body().withText(new Content(body)))
+                );
+        try {
+            this.amazonSimpleEmailService.sendEmail(request);
+        } catch (AmazonServiceException exception) {
+            throw new EmailServiceException("Failure while sending email");
+        }
+    }
+}
